af0ff1d53a34b213a38b9dce4b486aa52070dfab
qpx5997 <lamdieuphong14@gmail.com>
2026-02-17 16:43:38 +0800

    Changed tabs in the source code to spaces, fixed a bug, added a new chapter to the manual and added delp and addp commands

--- Monopoly_EBank.py
+++ Monopoly_EBank.py
@@ -2,33 +2,33 @@ print("""Monopoly E-Bank v1.0, a Python program by qpx5997
 Type "help" to get a quick command reference, or "manual" for how to use this program.""")
 
 money = {
-	"p1": 1500,
-	"p2": 1500,
-	"p3": 1500,
-	"p4": 1500
+    "p1": 1500,
+    "p2": 1500,
+    "p3": 1500,
+    "p4": 1500
 }
 
 while True:
-	cmd = input(">>> ")
-	cmd_split = cmd.split(" ") # Splits the command into multiple list items
-	if cmd_split[0] == "reset":
-		# reset money (to 1500), properties
-		money = {
-		"p1": 1500,
-		"p2": 1500,
-		"p3": 1500,
-		"p4": 1500
-	}
-		print("All player money has been reset to M1500.")
-		continue
-	
-	elif cmd_split[0] == "manual":
-		manual = [
-			"""Welcome to the Monopoly E-Bank Manual.
+    cmd = input(">>> ")
+    cmd_split = cmd.split(" ") # Splits the command into multiple list items
+    if cmd_split[0] == "reset":
+        # reset money (to 1500), properties
+        money = {
+        "p1": 1500,
+        "p2": 1500,
+        "p3": 1500,
+        "p4": 1500
+    }
+        print("All player money has been reset to M1500.")
+        continue
+    
+    elif cmd_split[0] == "manual":
+        manual = [
+            """Welcome to the Monopoly E-Bank Manual.
 This manual will teach you all of this CLI's commands.
 If you only need a quick overview, type "quit", an then "help".""",
 
-			"""1. COMMANDS FOR MANAGING MONEY
+            """1. COMMANDS FOR MANAGING MONEY
 reset: Resets all player money to M1500.
 add: Adds money to the stated player from the bank.
 EXAMPLE: add p1 50 -- Gives M50 to p1
@@ -38,7 +38,7 @@ go: Gives M200 to the stated player. Used as a shortcut from "add p1 200".
 EXAMPLE: go p1 -- Gives M200 to p1
 set: Sets the stated player's money to the stated amount.
 EXAMPLE: set p1 300 -- Sets p1's amount to M300.""",
-			"""trans: Transfers money from one player to another.
+            """trans: Transfers money from one player to another.
 EXAMPLE: trans p2 p4 10 -- Transfers M10 from p2 to p4.
 vm: Short for View Money; lets you see the stated player's the amount of money.
 EXAMPLE: vm p1 -- Shows the amount of money p1 has.
@@ -47,7 +47,7 @@ itax: Short for Income Tax; subtracts M200 from the stated player.
 EXAMPLE: itax p1 -- Subtracts M200 from p1.
 ltax: Short for Luxury Tax; subtracts M100 from the stated player.
 EXAMPLE: ltax p1 -- Subtracts M100 from p1.""",
-			"""2. OTHER MISCELLANEOUS COMMANDS
+			"""2. COMMANDS FOR PLAYER MANAGEMENT
 ren: Renames a player.
 EXAMPLE: ren p1 someone -- Renames p1 to someone.
 WARNING: If you type:
@@ -56,11 +56,17 @@ add p1 100
 It will show "ERROR: Nonexistent player "p1"".
 Instead, you have to type:
 add someone 100
+addp: Adds a player to the game.
+EXAMPLE: addp p5 1200 -- Adds player "p5" to the game with M1200.
+EXAMPLE: addp p6 -- Adds player "p6" to the game with M1500.
+delp: Removes a player from the game.
+EXAMPLE: delp p1 -- Removes player "p1" from the game.""",
+            """3. OTHER MISCELLANEOUS COMMANDS
 help: Shows a quick overview of the commands.
 manual: Shows a tutorial on how to use the program.
 changelog: Shows the program changelog.
 exit: Stops the program.""",
-			"""3. PHYSICAL MANAGEMENT
+            """3. PHYSICAL MANAGEMENT
 The following things still need to be managed physically on the board:
 - properties;
 - houses and hotels;
@@ -70,28 +76,30 @@ The following things still need to be managed physically on the board:
 - jail and get out of jail free cards;
 - mortgages;
 - trading of items between players (though payments can be managed in the CLI)"""
-		]
-		for paragraph in manual:
-			print(paragraph)
-			print("Press enter to continue or type \"quit\" to return to the CLI.")
-			mcmd = input(">>> ")
-			if mcmd == "quit":
-				break
-		print("""You are now leaving the manual and returning to the CLI.
+        ]
+        for paragraph in manual:
+            print(paragraph)
+            print("Press enter to continue or type \"quit\" to return to the CLI.")
+            mcmd = input(">>> ")
+            if mcmd == "quit":
+                break
+        print("""You are now leaving the manual and returning to the CLI.
 If at any time you need a quick review of the commands, type "help".""")
-		continue
-		
-	elif cmd_split[0] == "changelog":
-		print("""PROGRAM CHANGELOG
+        continue
+        
+    elif cmd_split[0] == "changelog":
+        print("""PROGRAM CHANGELOG
 v1.0 (26/10/2025): Initial release, can only handle money.
 v1.1 (29/10/2025): Added player renaming and fixed a minor spelling error. Also did a bugfix.
 v1.2 (31/10/2025): Did a bugfix.
-v1.3 (07/11/2025): Added ltax and itax commands and did a bugfix. <-- CURRENT VERSION
+v1.3 (07/11/2025): Added ltax and itax commands and did a bugfix.
+v1.4 (17/02/2026): A minor edit where I changed the header to my new username. Forgot to update the version history command print in the program itself tho :\
+v1.5 (17/02/2026): Changed tabs in the source code to spaces, fixed a bug, added a new chapter to the manual and added delp and addp commands. <-- CURRENT VERSION
 v2.0 (FUTURE): Add property management.""")
-		continue
-		
-	elif cmd_split[0] == "help":
-		print("""QUICK COMMAND HELP
+        continue
+        
+    elif cmd_split[0] == "help":
+        print("""QUICK COMMAND HELP
 reset: Resets all money and properties.
 add p1 100: Adds M100 to p1.
 subt p1 100: Subtracts M100 from p1.
@@ -102,76 +110,91 @@ vm p1: Shows p1's money.
 vm all: Shows every player's money.
 ren p1 someone: Renames p1 to someone.
 exit: Quits the program.""")
-		continue
-		
-	elif cmd_split[0] == "exit":
-		quit()
-	
-	try:
-		if cmd_split[0] == "add":
-			money[cmd_split[1]] += int(cmd_split[2]) # Adds money
-			print(f"""M{cmd_split[2]} added to {cmd_split[1]}.
+        continue
+        
+    elif cmd_split[0] == "exit":
+        quit()
+    
+    try:
+        if cmd_split[0] == "add":
+            money[cmd_split[1]] += int(cmd_split[2]) # Adds money
+            print(f"""M{cmd_split[2]} added to {cmd_split[1]}.
 {cmd_split[1]}'s current total money: M{money[cmd_split[1]]}""") # This print line displays command data; how much money was added, etc.
-		
-		elif cmd_split[0] == "subt":
-			money[cmd_split[1]] -= int(cmd_split[2])
-			print(f"""M{cmd_split[2]} subtracted from {cmd_split[1]}.
+        
+        elif cmd_split[0] == "subt":
+            money[cmd_split[1]] -= int(cmd_split[2])
+            print(f"""M{cmd_split[2]} subtracted from {cmd_split[1]}.
 {cmd_split[1]}'s current total money: M{money[cmd_split[1]]}""")
-			
-		elif cmd_split[0] == "trans":
-			money[cmd_split[1]] -= int(cmd_split[3])
-			money[cmd_split[2]] += int(cmd_split[3])
-			print(f"""M{cmd_split[3]} transferred from {cmd_split[1]} to {cmd_split[2]}.
+            
+        elif cmd_split[0] == "trans":
+            money[cmd_split[1]] -= int(cmd_split[3])
+            money[cmd_split[2]] += int(cmd_split[3])
+            print(f"""M{cmd_split[3]} transferred from {cmd_split[1]} to {cmd_split[2]}.
 {cmd_split[1]}'s current total money: M{money[cmd_split[1]]}
 {cmd_split[2]}'s current total money: M{money[cmd_split[2]]}""")
-		
-		elif cmd_split[0] == "go":
-			money[cmd_split[1]] += 200
-			print(f"""M200 added to {cmd_split[1]}.
-{cmd_split[1]}'s current total money: {money[cmd_split[1]]}""")
-		
-		elif cmd_split[0] == "set":
-			money[cmd_split[1]] = int(cmd_split[2])
-			print(f"""{cmd_split[1]}'s money set to M{cmd_split[2]}.""")
-	
-		elif cmd_split[0] == "vm":
-			if cmd_split[1] == "all":
-				print(f"""ALL PLAYER CURRENT TOTAL MONEY:
+        
+        elif cmd_split[0] == "go":
+            money[cmd_split[1]] += 200
+            print(f"""M200 added to {cmd_split[1]}.
+{cmd_split[1]}'s current total money: M{money[cmd_split[1]]}""")
+        
+        elif cmd_split[0] == "set":
+            money[cmd_split[1]] = int(cmd_split[2])
+            print(f"""{cmd_split[1]}'s money set to M{cmd_split[2]}.""")
+    
+        elif cmd_split[0] == "vm":
+            if cmd_split[1] == "all":
+                print(f"""ALL PLAYER CURRENT TOTAL MONEY:
 
 {money}""")
-			else:
-				print(f"""{cmd_split[1]}'s current total money: M{money[cmd_split[1]]}""")
+            else:
+                print(f"""{cmd_split[1]}'s current total money: M{money[cmd_split[1]]}""")
+                
+        elif cmd_split[0] == "ren": # The if-clauses exist because if it ran like normal and someone typed only 2 arguments the program would end up deleting the player
+            if len(cmd_split) < 3:
+                print(f"ERROR: Only {len(cmd_split)} argument(s) were given for command {cmd}")
+            else:
+                money[cmd_split[2]] = money.pop(cmd_split[1])
+                print(f"""{cmd_split[1]} renamed to {cmd_split[2]}.
+{cmd_split[2]}'s current total money: M{money[cmd_split[2]]}""")
 
-		elif cmd_split[0] == "ren":
-			money[cmd_split[2]] = money.pop(cmd_split[1])
-			print(f"""{cmd_split[1]} renamed to {cmd_split[2]}.
-{cmd_split[2]}'s current total money: {money[cmd_split[2]]}""")
+        elif cmd_split[0] == "itax":
+            money[cmd_split[1]] -= 200
+            print(f"""M200 subtracted from {cmd_split[1]}.
+{cmd_split[1]}'s current total money: M{money[cmd_split[1]]}""")
+            
+        elif cmd_split[0] == "ltax":
+            money[cmd_split[1]] -= 100
+            print(f"""M100 subtracted from {cmd_split[1]}.
+{cmd_split[1]}'s current total money: M{money[cmd_split[1]]}""")
 
-		elif cmd_split[0] == "itax":
-			money[cmd_split[1]] -= 200
-			print(f"""M200 subtracted from {cmd_split[1]}.
-{cmd_split[1]}'s current total money: {money[cmd_split[1]]}""")
-			
-		elif cmd_split[0] == "ltax":
-			money[cmd_split[1]] -= 100
-			print(f"""M100 subtracted from {cmd_split[1]}.
-{cmd_split[1]}'s current total money: {money[cmd_split[1]]}""")
+        elif cmd_split[0] == "addp":
+            if len(cmd_split) > 2:
+                money[cmd_split[1]] = int(cmd_split[2])
+            elif len(cmd_split) <= 2:
+                money[cmd_split[1]]  = 1500
+            print(f"Player "{cmd_split[1]}" added with M{money[cmd_split[1]]}.")
 
-		else:
-			print(f"ERROR: Unknown command \"{cmd_split[0]}\"")
-			
-	except KeyError:
-		if cmd_split[0] =="trans":
-			print(f"ERROR: Invalid transfer origin/destination \"{cmd_split[1]} {cmd_split[2]}\"")
-		else:
-			print(f"ERROR: Nonexistent player \"{cmd_split[1]}\"")
+        elif cmd_split[0] == "delp":
+            money.pop(cmd_split[1])
+            print(f"Player "{cmd_split[1]}" deleted.")
 
-	except ValueError:
-		if cmd_split[0] =="trans":
-			print(f"ERROR: Invalid money value \"{cmd_split[3]}\"")
-		else:
-			print(f"ERROR: Invalid money value \"{cmd_split[2]}\"")
+        else:
+            print(f"ERROR: Unknown command \"{cmd_split[0]}\"")
+            
+    except KeyError:
+        if cmd_split[0] =="trans":
+            print(f"ERROR: Invalid transfer origin/destination \"{cmd_split[1]} {cmd_split[2]}\"")
+        else:
+            print(f"ERROR: Nonexistent player \"{cmd_split[1]}\"")
 
-	except IndexError:
-		print(f"ERROR: Only {len(cmd_split)} argument(s) were given for command {cmd}")
+    except ValueError:
+        if cmd_split[0] =="trans":
+            print(f"ERROR: Invalid money value \"{cmd_split[3]}\"")
+        else:
+            print(f"ERROR: Invalid money value \"{cmd_split[2]}\"")
+
+    except IndexError:
+        print(f"ERROR: Only {len(cmd_split)} argument(s) were given for command {cmd}")
+
 
